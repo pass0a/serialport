9180ab22840184aa2b8311cafce5481bf4dd2b0b
liuwenjun <liuwenjun@hangsheng.com.cn>
2019-09-10 22:33:54 +0800

    add npm

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,5 @@
 import { serialport } from '../src/index';
-let s = new serialport('COM1');
+let s = new serialport('COM1', { baud_rate: 9600 });
 s.on('data', (data) => {
 	console.log(data);
 	s.write('nihao');
